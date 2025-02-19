a3640f4093867f032c0e37b705f407661d307006
Krystalcuarteros <charita121279@gmail.com>
2025-02-19 10:07:47 +0800

    Update Lab_act1_Arithmetics.java

--- Lab_act1_Arithmetics.java
+++ Lab_act1_Arithmetics.java
@@ -1,25 +1,25 @@
 package Prelim;
 
+import java.util.Scanner;
 
 public class Lab_act1_Arithmetics {
-
-    public static void main(String[] args) {
-       
-        int firstNumber = 10;
-        int secondNumber = 4;
-        int thirdNumber = 6;
-
+    public static void main(String[] args){
         
-        int result1 = firstNumber * secondNumber + thirdNumber;
-        int result2 = (firstNumber - secondNumber) % thirdNumber;
-        int result3 = (firstNumber + secondNumber + thirdNumber) / 3;
-        int result4 = firstNumber * thirdNumber - (secondNumber * secondNumber);
-
+        int first_number,second_number,third_number;
+        int x = 3;
        
-        System.out.println(firstNumber + " * " + secondNumber + " + " + thirdNumber + " = " + result1);
-        System.out.println("(" + firstNumber + " - " + secondNumber + ") % " + thirdNumber + " = " + result2);
-        System.out.println("(" + firstNumber + " + " + secondNumber + " + " + thirdNumber + ") / 3 = " + result3);
-        System.out.println(firstNumber + " * " + thirdNumber + " - (" + secondNumber + " * " + secondNumber + ") = " + result4);
+        Scanner in = new Scanner(System.in);
+        
+        System.out.print("Enter first number: ");
+        first_number = in.nextInt();
+        System.out.print("Enter second number: ");
+        second_number = in.nextInt();
+        System.out.print("Enter third number: ");
+        third_number = in.nextInt();
+        
+        System.out.println(first_number + " * " + second_number + " + " + third_number + " = " + (first_number * second_number + third_number));
+        System.out.println("(" + first_number + " - " + second_number + ")" + " % " + third_number + " = " + ((first_number - second_number) % third_number));
+        System.out.println("(" + first_number + " + " + second_number + " + " + third_number + ")" + " / " + x + " = " + ((first_number + second_number + third_number)/ x));
+        System.out.println(first_number + " * " + third_number + " - " + "(" + second_number + " * " + second_number + ")" +  " = " + (first_number * third_number - (second_number * second_number)));
     }
 }
-    
